f96cba6dc331a0eae003ecd180c726aabf65c26c
mikelbrierly <mikel@hey.com>
2020-12-15 19:06:08 -0600

    updating homework and lesson plan for js

---
+++ example.js
@@ -0,0 +1,88 @@
+// write an alert that welcomes users to your webpage
+
+
+
+// console log a string
+
+
+
+// console log a number
+// and make sure to open your browser dev tools to see the output in the console
+
+
+
+// create a (let) variable named myString and assign it to any string you like
+
+
+
+// create a (let) variable named myNum and assign it to any number you like
+
+
+
+// write two console log statements to log out myString and myNum
+
+
+
+// go back and comment out your alert statement at the beginning of this file
+
+
+
+// create a (const) variable and assign it to a number
+
+
+
+// re-assign myString to a different string and console log it again
+
+
+
+// using concatenation, console log both let variables and your const variable as a single string
+
+
+
+// use the strict equality operator to compare your two variables that are numbers. console log that out
+
+
+
+// add a console.log statement to this function that will log out the "name" parameter
+const printName = (name) => {
+  // code here
+};
+
+// invoke the printName function here and pass in your name as an argument
+// invoke (call) function here
+
+
+
+// update this function so that it adds 2 to any given number.
+const addTwo = () => {
+
+};
+
+// this should output 7
+console.log(addTwo(5)); // (don't change this line)
+
+// this should output 22
+console.log(addTwo(20)); // (don't change this line)
+
+
+
+// write a function named "squared" that takes in one parameter.
+
+
+
+// in the body (between the curly braces) of the function, multiply the parameter by itself.
+// make sure the function has a "return" statement
+
+
+
+// call/invoke your function and pass it the number 7
+console.log( /*invoke function here*/ );
+
+
+
+// write a function that re-assigns one of the previous let variables we created to the string "I'm a changed var"
+
+
+
+// uncomment the following line
+// alert("I'm a badass and I'm done!")
